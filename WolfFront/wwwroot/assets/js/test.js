85410a27e0e9d8a07f044e012256bee6b0d0ede5
Даниил Киркин <kirkin@sfedu.ru>
2020-05-14 21:05:19 +0300

    i love js

--- WolfFront/wwwroot/assets/js/test.js
+++ WolfFront/wwwroot/assets/js/test.js
@@ -9,6 +9,7 @@ if (token != null) {
         crossDomain: true,
         success: function () {
             $("#submit").click(function (event) {
+                event.preventDefault();
                 let correct = new Array();
                 $.ajax({
                     method: "Get",
@@ -19,57 +20,55 @@ if (token != null) {
                     crossDomain: true,
                     success: function (data) {
                         correct = data;
-                    }
-                });
-                event.preventDefault();
-                let answers = new Array();
-                let allChecked = true;
-                $(".answers").toArray().forEach(item => {
-                    let answered = false;
-                    for (let option of item.children) {
-                        if (option.getElementsByTagName('input')[0].checked) {
-                            answers.push(option.getElementsByTagName('label')[0].innerText);
-                            answered = true;
-                            return;
-                        }
-                    }
-                    if (!answered) {
-                        allChecked = false;
-                    }
-                });
-                if (allChecked) {
-                    let level = "Волчонок";
-                    let points = 0;
-                    //let correct = new Array("Будет ошибка", "NaN", "2", "false, true.", "код функции f", "undefined", "В коде ошибка", "Math.max.apply(null, arr)", "parent", "Обычным объектом", "0, затем 1", "дупкин", "Цифре", "#", "null");
-                    for (i = 0; i < 15; ++i) {
-                        if (answers[i] === correct[i]) {
-                            ++points;
-                        }
-                    }
-                    if (points > 3) {
-                        level = "Волчонок";
-                        if (points > 6) {
-                            level = "Шакал";
-                            if (points > 9) {
-                                level = "Волк";
-                                if (points > 12) {
-                                    level = "Оборотень";
-                                    if (points == 15) {
-                                        level = "Вожак стаи";
+                        let answers = new Array();
+                        let allChecked = true;
+                        $(".answers").toArray().forEach(item => {
+                            let answered = false;
+                            for (let option of item.children) {
+                                if (option.getElementsByTagName('input')[0].checked) {
+                                    answers.push(option.getElementsByTagName('label')[0].innerText);
+                                    answered = true;
+                                    return;
+                                }
+                            }
+                            if (!answered) {
+                                allChecked = false;
+                            }
+                        });
+                        if (allChecked) {
+                            let level = "Волчонок";
+                            let points = 0;
+                            for (i = 0; i < 15; ++i) {
+                                if (answers[i] === correct[i]) {
+                                    ++points;
+                                }
+                            }
+                            if (points > 3) {
+                                level = "Волчонок";
+                                if (points > 6) {
+                                    level = "Шакал";
+                                    if (points > 9) {
+                                        level = "Волк";
+                                        if (points > 12) {
+                                            level = "Оборотень";
+                                            if (points == 15) {
+                                                level = "Вожак стаи";
+                                            }
+                                        }
                                     }
                                 }
                             }
+                            console.log(points);
+                            let answer = "Ваш результат: " + points + " баллов из 15 \n";
+                            let levelstr = "Предполагаемый уровень: " + level;
+                            $('.cont-qestions').attr('hidden', true);
+                            $('section').append('<h2>' + answer + '</h2>' + '<h2>' + levelstr + '</h2>');
+                        }
+                        else {
+                            alert('Please, complete all the tasks');
                         }
                     }
-                    console.log(points);
-                    let answer = "Ваш результат: " + points + " баллов из 15 \n";
-                    let levelstr = "Предполагаемый уровень: " + level;
-                    $('.cont-qestions').attr('hidden',true);
-                    $('section').append('<h2>' + answer + '</h2>' + '<h2>' + levelstr + '</h2>');
-                }
-                else {
-                    alert('Please, complete all the tasks');
-                }
+                });
             })
         },
         error: function () {
